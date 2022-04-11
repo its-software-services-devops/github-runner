23df617a8ba9921d3126aa6c23769afb9653f6b9
seubpong.mon <pjame.fb@gmail.com>
2022-04-11 20:59:57 +0700

    Setup auto deploy mode

--- scripts/run-git-clone.bash
+++ scripts/run-git-clone.bash
@@ -5,5 +5,7 @@ echo "#### Running the ${0} script ####"
 CLONE_SSH_URI=$1
 TO_FOLDER=$2
 
-URL=https://gitlab-ci-token:${GITLAB_TOKEN}@gitlab.com/ever-medical-technologies/${CLONE_SSH_URI}
+#URL=https://gitlab-ci-token:${GITLAB_TOKEN}@gitlab.com/ever-medical-technologies/${CLONE_SSH_URI}
+
+URL=https://${GITHUB_TOKEN}:github.com/${CLONE_SSH_URI}
 git clone ${URL} ${TO_FOLDER}
