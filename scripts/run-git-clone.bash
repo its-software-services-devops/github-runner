6a67295d8b8d18afc400043a1a8798bebfe1f37f
seubpong.mon <pjame.fb@gmail.com>
2022-04-11 21:18:14 +0700

    Test github access token

--- scripts/run-git-clone.bash
+++ scripts/run-git-clone.bash
@@ -7,5 +7,6 @@ TO_FOLDER=$2
 
 #URL=https://gitlab-ci-token:${GITLAB_TOKEN}@gitlab.com/ever-medical-technologies/${CLONE_SSH_URI}
 
-URL=https://${GITHUB_TOKEN}:github.com/${CLONE_SSH_URI}
+GITHUB_TOKEN=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
+URL=https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${CLONE_SSH_URI}
 git clone ${URL} ${TO_FOLDER}
