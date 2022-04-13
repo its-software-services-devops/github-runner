0e1b66d7058c778d2aaf7239c2ba14c83775c050
seubpong.mon <pjame.fb@gmail.com>
2022-04-13 19:39:08 +0700

    Modify for github host

--- scripts/run-prepare.bash
+++ scripts/run-prepare.bash
@@ -3,8 +3,15 @@
 GCR=asia.gcr.io
 GAR=asia-southeast1-docker.pkg.dev
 VERSION=$(cat /home/runner/version.txt)
+KEYFILE_DIR=$(dirname ${GOOGLE_APPLICATION_CREDENTIALS})
 
 echo "#### Running the ${0} script, docker version [${VERSION}] ####"
+
+JSON=${GOOGLE_APPLICATION_CREDENTIALS_JSON} #Injected by Github Secret
+if [ "${JSON}" != '' ]; then
+    mkdir -p ${KEYFILE_DIR}
+    echo "${JSON}" | base64 -d > ${GOOGLE_APPLICATION_CREDENTIALS}
+fi
 
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin ${GCR}
