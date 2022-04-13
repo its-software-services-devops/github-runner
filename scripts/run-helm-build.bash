ac6a998e74e8d9c53b7c50adf456e883f33ed23f
seubpong.mon <pjame.fb@gmail.com>
2022-04-13 08:33:53 +0700

    Improve helm build

--- scripts/run-helm-build.bash
+++ scripts/run-helm-build.bash
@@ -4,6 +4,7 @@
 
 CHART_PATH=$1
 CHART_NAME=$2
+ALIAS=$2
 
 echo "#### Running the ${0} script ####"
 
@@ -11,9 +12,9 @@ GCS_PATH=gs://evermed-devops-helm-charts/${CHART_NAME}
 
 helm gcs init ${GCS_PATH}
 helm dependencies update
-helm repo add ${CHART_PATH} ${GCS_PATH}
+helm repo add ${ALIAS} ${GCS_PATH}
 
 helm package ${CHART_PATH} #--version="${VERSION}"
 
 PKG_FILE=$(ls ${CHART_PATH}/helm*.tgz)
-helm gcs push ${PKG_FILE} ${CHART_PATH} --force
+helm gcs push ${PKG_FILE} ${ALIAS} --force
